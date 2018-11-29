dc8b95f28752a6c677d12d5786c0a8b60945a017
seoyoonbaek <bsj8993@naver.com>
2018-11-29 20:20:35 +0900

    add pbk_mymote01

---
+++ pbk_mymote01/src/pbk_mymote/Appointment.java
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+package pbk_mymote;
+
+public class Appointment {
+
+}
+=======
+package pbk_mymote;
+
+public class Appointment {
+
+}
+>>>>>>> a06ec88016b0fd5bfe429f72b92d121793d737bc
