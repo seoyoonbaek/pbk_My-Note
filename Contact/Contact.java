2702d389940291110edfc90f2f4431c44c4a01cc
kryoustar@gmail.com <kryoustar@gmail.com>
2018-12-05 01:59:35 +0900

    Add Contact/

--- Contact/Contact.java
+++ Contact/Contact.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.util.Scanner;
 import java.io.*;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Contact {
 	static Scanner scan = new Scanner(System.in);
 	public String Name, PhoneNumber, Email;
-		static HashMap<String,Contact> ContactMap = new HashMap<String,Contact>();
+		static ArrayList<Contact> ContactMap = new ArrayList<Contact>();
 	
 	static void createContact(String Name, String PhoneNumber, String Email) {
 		/*System.out.print("이름: ");
@@ -16,8 +16,9 @@ public class Contact {
 		System.out.print("이메일: ");
 		String temp3 = scan.nextLine();
 		Contact c = new Contact(temp1,temp2,temp3);*/
-		Contact contact = new Contact(Name, PhoneNumber,Email);
-		ContactMap.put(Name, contact);
+		Contact contactTemp = new Contact(Name, PhoneNumber,Email);
+		ContactMap.add(contactTemp);
+		
 	}
 	static void saveContact() throws FileNotFoundException, IOException {
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Contact.txt"));
@@ -28,55 +29,70 @@ public class Contact {
 	static void openContactFile() throws ClassNotFoundException {
 		try {
 			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Contact.txt"));
-			ContactMap = (HashMap<String,Contact>)ois.readObject();
+			ContactMap = (ArrayList<Contact>)ois.readObject();
 			ois.close();
 		}
 		catch(FileNotFoundException fnf){
 			System.out.println("파일을 찾지 못하였습니다.");
 		}
 		catch(IOException e) {
+			System.out.println("에러가 나버렸습니다."); //나중에 지울코듸;
 			System.out.println(e);
 		}
 		
 	}
-	static void viewContact() {
+	static void viewContactList() {
 		if(ContactMap.isEmpty()) {
 			System.out.println("저장된 연락처가 없습니다.");
 			return ;
 		}
-		Set<String> keys = ContactMap.keySet();
-		Iterator<String> it = keys.iterator();
-		while(it.hasNext()) {
-			String name = it.next();
-			Contact details = ContactMap.get(name);
-			System.out.println("이름 : "+details.Name+"\t연락처 : "+details.PhoneNumber+"\t이메일 : "+details.Email );
+		else {
+			for(int index = 0; index<ContactMap.size();index++) {
+				int number = index +1;
+				System.out.println(number +". 이름 : "+ContactMap.get(index).Name+"\n");
+			}
 		}
 	}
-	static void deleteContact(String key) {
-		if (ContactMap.containsKey(key)) {
-			ContactMap.remove(key);
+	/* 여기서 뷰탭 들어가서는 이름만 촤라라 나오게 할건지 아니면 이름 전화번호 이메일 다 촤라라 나오게 하고 바로 업데이트 딜리트 메뉴 띠울건지,,,
+	static void viewContactDetails(int Number) {
+		if(ContactMap.isEmpty()) {
+			System.out.println("저장된 연락처가 없습니다.");
+		}
+		else {
+				int index = Number -1;
+				System.out.println("이름 : "+ContactMap.get(index).Name+"\t연락처 : "+ContactMap.get(index).PhoneNumber+"\t이메일 : "+ContactMap.get(index).Email+"\n");
+			
+		}
+	}*/ //제가 지우겠습니다;
+	static void deleteContact(int number) {
+		if (number <= ContactMap.size()) {
+			int index = number-1;
+			ContactMap.remove(index);
 		}
 		else {
 			System.out.println("저장된 항목이 없습니다.");
 			return;
-		
 		}
 	}
-	static void updateContact(String key) {
-		if (ContactMap.containsKey(key)) {
-			String tempName = ContactMap.get(key).Name;
-			String tempPhoneNumber = ContactMap.get(key).PhoneNumber;
-			String tempEmail = ContactMap.get(key).Email;
+	static void updateContact(int index) {
+		index=index-1;
+		if (index < ContactMap.size()) {
+			String tempName = ContactMap.get(index).Name;
+			String tempPhoneNumber = ContactMap.get(index).PhoneNumber;
+			String tempEmail = ContactMap.get(index).Email;
+			
 			System.out.print("이름: ");
 			String temp1 = scan.nextLine();
-			if (!temp1.equals("")) {tempName = temp1;}
 			System.out.print("전화번호: ");
 			String temp2 = scan.nextLine();
-			if(!temp2.equals("")) {tempPhoneNumber = temp2;}
 			System.out.print("이메일: ");
 			String temp3 = scan.nextLine();
+			
+			if (!temp1.equals("")) {tempName = temp1;}
+			if(!temp2.equals("")) {tempPhoneNumber = temp2;}
 			if(!temp3.equals("")) {tempEmail = temp3;}
-			ContactMap.remove(key);
+			
+			ContactMap.remove(index);
 			Contact.createContact(tempName, tempPhoneNumber, tempEmail);
 		}
 		else {
--- Contact/Main.java
+++ Contact/Main.java
@@ -15,19 +15,35 @@ import java.io.FileReader;
 import java.io.IOException;
 
 
-public class Main extends Contact {
-	public Main(String Name, String PhoneNumber, String Email) {
+public class main extends Contact {
+	public main(String Name, String PhoneNumber, String Email) {
 		super(Name, PhoneNumber, Email);
 		// TODO Auto-generated constructor stub
 	}
 	public static String mainMenu() {
 		return "1. Contact 2. TodoList 3. Appointment 4. Quit ";
 	}
+	static String ContactTab;
+	static String TodoListTab;
+	static String AppointmentTab;
+	static String QuitTab;
 	public static String subMenu() {
 		return "1. Create 2. View 3. Save \t 0. Go to Main";
 	}
-	public static String ViewSubMenu() {
+	static String CreateTab;
+	static String ViewTab;
+	static String SaveTab;
+	static String GoToMainTab;
+	
+	public static String viewSubMenu() {
 		return "1. Update 2. Delete 0. Go Back";
+	}
+	static String UpdateTab;
+	static String DeleteTab;
+	static String GoBackTab;
+	
+	public static String deleteCancel() {
+		return "정말로 삭제하시겠습니까? y/n ";
 	}
 	
 	
@@ -38,11 +54,15 @@ public class Main extends Contact {
 		//HashMap<String,TodoList> TodoListMap = new HashMap<String,TodoList>();
 		//HashMap<String,Appointment> AppointmentMap = new HashMap<String,Appointment>();
 		
+		ContactTab = CreateTab = UpdateTab = "1";
+		TodoListTab = ViewTab = DeleteTab = "2";
+		AppointmentTab = SaveTab = "3";
+		GoToMainTab = GoBackTab = "0";
 		while(true) {
 			System.out.println(mainMenu());
 			String MainMenu;
 			MainMenu = scan.nextLine();
-			if (MainMenu.equals("1")) {
+			if (MainMenu.equals(ContactTab)) {
 				try {
 					Contact.openContactFile();
 					System.out.println("연락처를 불러왔습니다.");
@@ -54,7 +74,7 @@ public class Main extends Contact {
 					System.out.println(subMenu());
 					String SubMenu;
 					SubMenu = scan.nextLine();
-					if(SubMenu.equals("1")) {
+					if(SubMenu.equals(CreateTab)) {
 						System.out.print("이름: ");
 						String Name = scan.nextLine();
 						System.out.print("전화번호: ");
@@ -65,28 +85,37 @@ public class Main extends Contact {
 						System.out.println("저장되었습니다.\n");
 						continue;
 					}
-					if(SubMenu.equals("2")) {
+					if(SubMenu.equals(ViewTab)) {
 						System.out.println("========");
-						Contact.viewContact();
+						Contact.viewContactList();
 						System.out.println("========");
 						
-						System.out.println(ViewSubMenu());
+						System.out.println(viewSubMenu());
 						String ViewSubMenu;
 						ViewSubMenu = scan.nextLine();
-						if (ViewSubMenu.equals("1")) {
-							System.out.println("수정하실 이름을 작성해주세요.");
-							String key = scan.nextLine();
-							Contact.updateContact(key);
+						if (ViewSubMenu.equals(UpdateTab)) {
+							System.out.println("수정하실 번호를 작성해주세요.");
+							int index = scan.nextInt();
+							Contact.updateContact(index);
+							System.out.println("수정되었습니다.");
 						}
-						if (ViewSubMenu.equals("2")) {
-							String key = scan.nextLine();
-							Contact.deleteContact(key);
-							System.out.println("삭제되었습니다.");
+						if (ViewSubMenu.equals(DeleteTab)) {
+							System.out.println("삭제하실 번호를 작성해주세요.");
+							int index = scan.nextInt();
+							System.out.println(deleteCancel());
+							String choice = scan.nextLine();
+							if(choice.equals("y")) {
+								Contact.deleteContact(index);
+								System.out.println("삭제되었습니다.");
+							}
+							else {
+								System.out.println("취소되었습니다.");
+							}
 						}
 						
 						
 					}
-					if(SubMenu.equals("3")) {
+					if(SubMenu.equals(SaveTab)) {
 						try {
 							Contact.saveContact();
 						} catch (FileNotFoundException e) {
@@ -98,15 +127,15 @@ public class Main extends Contact {
 						}
 						
 					}
-					else {break;}
+					else {continue;}
 					
 				}
 				
 			}
-			if (MainMenu.equals("2")) {
+			if (MainMenu.equals(TodoListTab)) {
 				
 			}
-			if (MainMenu.equals("3")) {
+			if (MainMenu.equals(AppointmentTab)) {
 				
 			}
 			else	{
